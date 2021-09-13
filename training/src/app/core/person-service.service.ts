68eca2c8afe39ea5d9c2efcdd3a172ab86c1adcc
Sanjai <Sanjai.com@gmail.com>
2021-09-13 01:17:10 -0400

    day 2 -- set 2

--- training/src/app/core/person-service.service.ts
+++ training/src/app/core/person-service.service.ts
@@ -1,25 +1,38 @@
+import { HttpClient } from '@angular/common/http';
 import { ThrowStmt } from '@angular/compiler';
 import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
+import {map} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PersonServiceService {
   personsSubject = new Subject<string[]>();
-  personList: string[] =['tara','amy','jim'];
-  constructor() { }
-  addPerson(name:string){
+  personList: string[] = ['tara', 'amy', 'jim'];
+  constructor(private http: HttpClient) { }
+  addPerson(name: string) {
     this.personList.push(name);
     console.log(this.personList);
     this.personsSubject.next(this.personList);
   }
-  removePerson(name:string){
+  removePerson(name: string) {
     console.log('name tobe removed', name);
-    this.personList = this.personList.filter(person=>{
+    this.personList = this.personList.filter(person => {
       return person !== name;
     })
     console.log(this.personList);
     this.personsSubject.next(this.personList);
+  }
+  fetchPersons() {
+    this.http.get<any>('https://swapi.dev/api/people/')
+    .pipe(map(res => {
+        return res.results.map(char => char.name);
+      })
+    ).subscribe(transfomedRes => {
+      console.log(transfomedRes);
+      this.personsSubject.next(transfomedRes);
+
+    })
   }
 }
--- training/src/app/home/WithSubjects/plist/plist.component.ts
+++ training/src/app/home/WithSubjects/plist/plist.component.ts
@@ -5,19 +5,24 @@ import { PersonServiceService } from 'src/app/core/person-service.service';
 
 @Component({
   selector: 'app-plist',
-  template: '<p *ngFor="let person of plist">{{person}}</p>',
+  template: '<p *ngIf="isFetching === true">Loading....</p><div *ngIf="isFetching=== false"><p *ngFor="let person of plist">{{person}}</p></div>',
   styleUrls: ['./plist.component.scss'],
 })
 export class PlistComponent implements OnInit, OnDestroy {
   private perSubscription: Subscription;
   plist : string[] = ['appa','amma','mama','athai'];
+  isFetching:boolean = true;
   constructor(private plistsvc:PersonServiceService) { }
 
   ngOnInit() {
     this.plist = this.plistsvc.personList;
+
     this.perSubscription = this.plistsvc.personsSubject.subscribe(prs =>{
       this.plist = prs;
-    })
+      this.isFetching = false;
+    });
+    this.isFetching = true;
+    this.plistsvc.fetchPersons();
   }
 
   ngOnDestroy(){
